f46bcb6836e6a7a2f8a4ffc90e19ab51cc55466a
ReyesCS <reyes1561@yahoo.com>
2017-09-11 09:23:57 -0700

    First Commit

---
+++ Century/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Century
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+
+            //int.TryParse((Console.ReadLine()),out year);
+            int year = 2000;
+                if (year % 100 >= 1)
+                {
+                Console.WriteLine((year / 100) + 1);
+                }
+                else
+                {
+                Console.WriteLine(year / 100);
+                }
+
+            }
+
+        }
+    }
